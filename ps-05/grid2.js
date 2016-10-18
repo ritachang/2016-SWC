35b5f04aba9cf3371666d085a09cae6b1441c933
ritachang <r810288@gmail.com>
2016-10-18 17:04:00 -0400

    ps-05

---
+++ ps-05/grid2.js
@@ -0,0 +1,262 @@
+var canvas = document.getElementById('canvas');
+    var drawingPad = canvas.getContext('2d');
+   
+
+ var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#008b8b");
+   drawingPad.fillStyle=my_gradient;
+
+// drawingPad.fillStyle="yellow";
+drawingPad.fillRect(0,0,500,100);
+drawingPad.transform(1,-0.5,0,1,0,0);
+drawingPad.rotate(2*Math.PI/180);
+
+
+
+
+// drawingPad.fillStyle=red";
+drawingPad.fillRect(0,0,500,100);
+// drawingPad.scale(1.5,1.5);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(4*Math.PI/180);
+var my_gradient=drawingPad.createLinearGradient(0,0,100,90);
+       
+   var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#008080");
+   drawingPad.fillStyle=my_gradient;
+
+
+
+// drawingPad.fillStyle="blue";
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(2*Math.PI/180);
+var my_gradient=drawingPad.createLinearGradient(0,0,95,85);
+   
+
+
+
+// drawingPad.fillStyle="yellow";
+drawingPad.fillRect(20,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(1*Math.PI/180);
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(1*Math.PI/180);
+var my_gradient=drawingPad.createLinearGradient(0,0,150,100);
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#2e8b57");
+   drawingPad.fillStyle=my_gradient;
+
+// drawingPad.fillStyle="red";
+drawingPad.fillRect(20,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(1*Math.PI/180);
+drawingPad.fillRect(0,0,600,30);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(1*Math.PI/180);
+var my_gradient=drawingPad.createLinearGradient(0,0,100,150);
+
+
+// drawingPad.fillStyle="blue";
+drawingPad.fillRect(20,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(1*Math.PI/180);
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#3cb371");
+   drawingPad.fillStyle=my_gradient;
+
+
+
+
+// drawingPad.fillStyle="ellow";
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+var my_gradient=drawingPad.createLinearGradient(0,0,130,115);
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#006400");
+   drawingPad.fillStyle=my_gradient;
+
+
+// drawingPad.fillStyle="d";
+drawingPad.fillRect(30,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+var my_gradient=drawingPad.createLinearGradient(0,0,130,110);
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#008000");
+   drawingPad.fillStyle=my_gradient;
+
+
+
+// drawingPad.fillStyle="blue";
+drawingPad.fillRect(30,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+
+
+
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+drawingPad.rotate(1*Math.PI/180);
+var my_gradient=drawingPad.createLinearGradient(0,0,120,110);
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#228b22");
+   drawingPad.fillStyle=my_gradient;
+
+
+// drawingPad.fillStyle="yellow";
+drawingPad.fillRect(40,0,600,100);
+
+drawingPad.transform(1,0.1,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,110,105);
+
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+
+var my_gradient=drawingPad.createLinearGradient(0,0,105,100);
+   my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#3cb371");
+   drawingPad.fillStyle=my_gradient;
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+ 
+// drawingPad.fillStyle="red";
+drawingPad.fillRect(40,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,100,95);
+
+
+// drawingPad.fillStyle="blue";
+drawingPad.fillRect(40,0,600,100);
+drawingPad.transform(1,0.1,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,95,90);
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#66cdaa");
+   drawingPad.fillStyle=my_gradient;
+
+
+
+
+// drawingPad.fillStyle="black";
+drawingPad.fillRect(0,0,600,100);
+var my_gradient=drawingPad.createLinearGradient(0,0,90,85);
+
+
+drawingPad.transform(1,-0.2,0,1,0,0);
+
+// drawingPad.fillStyle="red";
+drawingPad.fillRect(0,0,600,100);
+drawingPad.transform(1,-0.2,0,1,0,0);
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,90,80);
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#98fb98");
+   drawingPad.fillStyle=my_gradient;
+
+   
+
+drawingPad.fillRect(0,80,600,30);
+drawingPad.transform(1,0.2,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,50,100);
+
+drawingPad.fillRect(0,90,600,30);
+drawingPad.transform(1,0.2,0,1,0,0);
+
+
+var my_gradient=drawingPad.createLinearGradient(0,0,40,110);
+drawingPad.fillRect(0,95,600,35);
+drawingPad.transform(1,0.2,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,45,115);
+drawingPad.fillRect(0,98,600,35);
+drawingPad.transform(1,0.2,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,40,105);
+
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(0,"#98fb98"); 
+    my_gradient.addColorStop(1,"white");
+   drawingPad.fillStyle=my_gradient;
+
+
+drawingPad.fillRect(0,100,600,70);
+drawingPad.transform(1,-1,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,200,0);
+
+drawingPad.fillRect(0,110,600,70);
+drawingPad.transform(1,-0.2,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,200,0);
+
+drawingPad.fillRect(0,120,600,60);
+drawingPad.transform(1,0.2,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,100,100);
+       
+    my_gradient.addColorStop(1,"white"); 
+    my_gradient.addColorStop(0,"#7cfc00");
+   drawingPad.fillStyle=my_gradient;
+
+drawingPad.fillRect(0,95,600,60);
+drawingPad.transform(1,0,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,150,0);
+drawingPad.fillRect(0,80,600,50);
+drawingPad.transform(1,0,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,120,0);
+
+drawingPad.fillRect(0,95,600,60);
+drawingPad.transform(1,0.1,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,150,0);
+
+drawingPad.fillRect(0,95,600,60);
+drawingPad.transform(1,0.2,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,150,0)
+drawingPad.fillRect(0,150,600,60);
+drawingPad.transform(1,0,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,150,0)
+
+drawingPad.fillRect(0,200,600,70);
+drawingPad.transform(1,0,0,1,0,0);
+var my_gradient=drawingPad.createLinearGradient(0,0,150,0)
+
+
+
